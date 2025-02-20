a79563a655e66fccbe389d5e1b06980176f77c5f
Trinh-Nguyen-Ngoc-Tu <nguyenngoctutrinh92@gmail.com>
2025-02-20 20:56:13 +0700

    Update Logger.cs

--- MovieSeries/CommonLayer/Logger.cs
+++ MovieSeries/CommonLayer/Logger.cs
@@ -1,1 +1,13 @@
-﻿
+using System;
+
+namespace MovieSeries.CommonLayer.Utilities
+{
+    public static class Logger
+    {
+        // Ghi log đơn giản ra console với thời gian hiện tại
+        public static void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+}﻿
