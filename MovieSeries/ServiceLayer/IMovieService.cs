0087d91b86245c0f4b1a7e626c18dc5819ade561
Trinh-Nguyen-Ngoc-Tu <nguyenngoctutrinh92@gmail.com>
2025-02-20 20:11:58 +0700

    push service layer

---
+++ MovieSeries/ServiceLayer/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿using Pj.CoreLayer.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pj.ServiceLayer.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<Movie>> GetAllMoviesAsync();
+        Task<Movie> GetMovieByIdAsync(int id);
+        Task AddMovieAsync(Movie movie);
+        Task UpdateMovieAsync(Movie movie);
+        Task DeleteMovieAsync(int id);
+
+        // Gọi Stored Procedure top-rated
+        Task<IEnumerable<Movie>> GetTopRatedMoviesWithSpAsync(int topCount);
+    }
+}
---
+++ MovieSeries/ServiceLayer/MovieService.cs
@@ -0,0 +1,48 @@
+﻿using Pj.CoreLayer.Entities;
+using Pj.RepositoryLayer.Interfaces;
+using Pj.ServiceLayer.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pj.ServiceLayer.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _movieRepository.GetAllAsync();
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            return await _movieRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddMovieAsync(Movie movie)
+        {
+            // Có thể thêm logic kiểm tra/validate (vd: trùng tiêu đề) tại đây
+            await _movieRepository.AddAsync(movie);
+        }
+
+        public async Task UpdateMovieAsync(Movie movie)
+        {
+            await _movieRepository.UpdateAsync(movie);
+        }
+
+        public async Task DeleteMovieAsync(int id)
+        {
+            await _movieRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Movie>> GetTopRatedMoviesWithSpAsync(int topCount)
+        {
+            return await _movieRepository.GetTopRatedMoviesWithSpAsync(topCount);
+        }
+    }
+}
