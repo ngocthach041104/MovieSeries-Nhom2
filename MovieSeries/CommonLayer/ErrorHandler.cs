7aa6e17147a53ae7b93cd59ab1955f30c4bb5afc
ngocthach041104 <thoitranngocthach30@gmail.com>
2025-02-20 20:53:08 +0700

    Add common layer and some classes

---
+++ MovieSeries/CommonLayer/ErrorHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MovieSeries.CommonLayer.Utilities
+{
+    public static class ErrorHandler
+    {
+        // Trả về thông báo lỗi đơn giản từ exception (có thể tích hợp logging, custom message, v.v.)
+        public static string GetErrorMessage(Exception ex)
+        {
+            // Ở đây bạn có thể thực hiện log lỗi hoặc xử lý phức tạp hơn nếu cần
+            return ex.Message;
+        }
+    }
+}
