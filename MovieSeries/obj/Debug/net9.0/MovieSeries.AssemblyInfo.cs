5381d401db8407810cb3157cccab57dcef39bbe0
ngocthach041104 <thoitranngocthach30@gmail.com>
2025-02-20 20:03:49 +0700

    1 commit appetting.json

--- MovieSeries/obj/Debug/net9.0/MovieSeries.AssemblyInfo.cs
+++ MovieSeries/obj/Debug/net9.0/MovieSeries.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSeries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbe755b0d93e5982964e847875648582ffa193a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSeries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSeries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
