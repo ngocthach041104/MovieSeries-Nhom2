1038c1f9ec30baef9a03c50e928ebc197575e6e6
VuaVungCao <125594828+j1mmyhvstle@users.noreply.github.com>
2025-02-20 20:54:54 +0700

    Update Validator.cs

--- MovieSeries/CommonLayer/Validator.cs
+++ MovieSeries/CommonLayer/Validator.cs
@@ -1,1 +1,11 @@
-﻿
+namespace MovieSeries.CommonLayer.Utilities
+{
+    public static class Validator
+    {
+        // Kiểm tra email có hợp lệ không: đơn giản chỉ là kiểm tra có chứa "@" và "."
+        public static bool IsValidEmail(string email)
+        {
+            return !string.IsNullOrEmpty(email) && email.Contains("@") && email.Contains(".");
+        }
+    }
+}﻿
