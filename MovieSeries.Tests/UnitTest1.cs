5e41044036b6ab3a371e7c69a7a38593138edb96
ngocthach041104 <thoitranngocthach30@gmail.com>
2025-02-20 21:23:09 +0700

    Add xUnit

---
+++ MovieSeries.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieSeries.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TestProject1/ValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using MovieSeries.CommonLayer.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieSeries.Tests.Utilities
+{
+    public class ValidatorTests
+    {
+        [Theory]
+        [InlineData("test@example.com", true)]
+        [InlineData("invalidemail", false)]
+        [InlineData("another@test", false)]
+        [InlineData("", false)]
+        public void IsValidEmail_Test(string email, bool expected)
+        {
+            // Act
+            bool result = Validator.IsValidEmail(email);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
